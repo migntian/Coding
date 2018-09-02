12ed9bd86d7440dd9b523070f1d05251e8e8edc4
tian <827746283@qq.com>
2018-09-02 09:25:49 +0800

    test2

---
+++ C++/test2/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //int i = 1;
+    //double j = 1.2;
+    //cout << i << j << endl;
+    //cin >> i >> j;
+    //cout << i << j<< endl;
+    
+}
